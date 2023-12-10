93426181a3dea4d8a256e4134dfd59bd4d34c4e4
Guilherme Santos <guilhermesantos2004gss@gmail.com>
2023-12-10 14:24:26 -0300

    Update

---
+++ tutorial.py
@@ -0,0 +1,11 @@
+def math(*args):
+    var = (number1 + number2)
+    return var
+
+name= str(input("DIGITE O SEU NOME: "));
+age = int(input("DIGITE SUA IDADE: "));
+number1 = int(input("DIGITE O 1 NÚMERO: "));
+number2 = int(input("DIGITE O 2 NÚMERO: "));
+print(f"O resultado dos valores digitados: {math(number1,number2)}");
+print(f"Bem vindo {name}!")
+print(f"Você nem parece que tem {age}")
